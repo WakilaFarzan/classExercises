12c8893469092419cea22dc80880ce71b6ca354b
WakilaFarzan <wakilatunnessachow.01@gmail.com>
2014-12-11 22:43:00 +0600

    Case insensitive sorting

---
+++ Create_function/example_17.php
@@ -0,0 +1,9 @@
+
+<?php
+$array = array('Alpha', 'atomic', 'Beta', 'bank');
+$array_lowercase = array_map('strtolower', $array);
+
+array_multisort($array_lowercase, SORT_ASC, SORT_STRING, $array);
+
+print_r($array);
+?>
