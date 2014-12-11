6a6e99bd08bed2e9e6319227fc853f9ba90abeab
WakilaFarzan <wakilatunnessachow.01@gmail.com>
2014-12-11 22:37:27 +0600

    database results

---
+++ Create_function/example_15.php
@@ -0,0 +1,9 @@
+
+<?php
+$data[] = array('volume' => 67, 'edition' => 2);
+$data[] = array('volume' => 86, 'edition' => 1);
+$data[] = array('volume' => 85, 'edition' => 6);
+$data[] = array('volume' => 98, 'edition' => 2);
+$data[] = array('volume' => 86, 'edition' => 6);
+$data[] = array('volume' => 67, 'edition' => 7);
+?>
